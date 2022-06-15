a079373f75e8bad4f5e75c2d94c0d8b85cfd98d4
Santiago Jose Orozco Guayara <santiago.orozco@globant.com>
2022-06-15 16:16:52 -0500

    Update and fix navigation with control of toolbar

--- app/src/main/java/com/example/shoppingapp/core/MainActivity.kt
+++ app/src/main/java/com/example/shoppingapp/core/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.shoppingapp.core
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.shoppingapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragmentContainerView)
+        return navController.navigateUp()
+                || super.onSupportNavigateUp()
     }
 }
