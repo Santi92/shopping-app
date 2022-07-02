ddf4d0cf585aab92412a8fdbee2d2523e7a6fb6c
Santiago Jose Orozco Guayara <santiago.orozco@globant.com>
2022-07-01 21:48:59 -0500

    Add unit test for get a error

--- app/src/test/java/com/example/shoppingapp/data/repository/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/example/shoppingapp/data/repository/ProductRepositoryImplTest.kt
@@ -1,13 +1,8 @@
 package com.example.shoppingapp.data.repository
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.example.shoppingapp.data.mapper.toModel
 import com.example.shoppingapp.data.remote.ProductApiService
 import com.example.shoppingapp.data.remote.ProductDTO
-import com.example.shoppingapp.domain.model.ProductItem
-import com.example.shoppingapp.domain.usecase.GetProductList
-import com.example.shoppingapp.presentation.product.list.ProductsUiState
-import com.example.shoppingapp.presentation.product.list.ProductsViewModel
 import com.example.shoppingapp.util.MainDispatcherRule
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
@@ -19,11 +14,6 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TestWatcher
-import org.junit.runner.Description
-
-
-
 
 @ExperimentalCoroutinesApi
 class ProductRepositoryImplTest {
@@ -51,7 +41,7 @@ class ProductRepositoryImplTest {
 
 
     @Test
-    fun `when call onCreate should get a product list`() = runTest(dispatcher) {
+    fun `when call getAllQuotes should get a product list`() = runTest(dispatcher) {
         //Given
         val item = ProductDTO("any", "any", 0.0,"any", "any","any")
         val list = listOf(item)
@@ -67,6 +57,24 @@ class ProductRepositoryImplTest {
         //Then
         assert(result.isSuccess)
         assert(resultList!! == list.map { it.toModel() })
+    }
+
+    @Test
+    fun `when call getAllQuotes should get a error`() = runTest(dispatcher) {
+        //Given
+        val item = ProductDTO("any", "any", 0.0,"any", "any","any")
+        coEvery { productApiService.getAllQuotes(any()) } returns  Result.failure(NullPointerException("Error occurred"))
+
+        //When
+        val result = productRepositoryImpl.getProducts()
+
+        advanceUntilIdle() // R
+
+        val resultList = result.getOrNull()
+
+        //Then
+        assert(result.isFailure)
+        assert(resultList == null)
     }
 
 
